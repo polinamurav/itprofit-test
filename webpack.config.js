11a53bb6c79fed30ccccfb23addc276cd0bfbb80
Polina Muravieva <polinamur3@gmail.com>
2025-03-29 19:31:22 +0300

    project structure

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    mode: 'development',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    resolve: {
+        alias: {
+            Styles: path.resolve(__dirname, 'src/styles/')
+        },
+        extensions: ['.js', '.scss']
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CopyPlugin({
+                patterns: [
+                    {from: "./src/js", to: "js"},
+                    // {from: "./node_modules/jquery/dist/jquery.min.js", to: "js"},
+                    {from: "./node_modules/inputmask/dist/inputmask.min.js", to: "js"},
+                ],
+            }
+        ),
+    ]
+};
